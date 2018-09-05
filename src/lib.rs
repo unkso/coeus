87ccec32ada3ace027f024a0362e0b32e81fb212
Eric Dattore <eric@dattore.me>
2018-09-05 17:27:42 -0600

    Created HTML parser module

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,17 @@
 #![allow(proc_macro_derive_resolution_fallback)]
 
 #[macro_use] extern crate diesel;
+extern crate regex;
 
+/// Parses the fetched sources for identifying lists of players
 mod parser;
-mod scraper;
+/// Fetches the source that's parsed for identifying lists of players
+mod fetcher;
+/// Contains functionality relating to creating and deleting Cohorts of playhers
 mod cohort;
+/// Contains functionality relating to identifying candidates for the Unknown Soldiers
 mod identifier;
+/// Contains the data models that are persisted
 mod models;
+/// Contains the description of the data persistence layer
 mod schema;
---
+++ src/parser.rs
@@ -0,0 +1,92 @@
+use std::io;
+use regex::{self, Regex};
+
+pub struct Parser {
+    name_regex: Regex,
+}
+
+impl Parser {
+    pub fn new() -> Result<Self, regex::Error> {
+        let name_regex = Regex::new(r#"<div class="player">\s*<a.*>(?P<name>.*)</a>\s*</div>"#)?;
+
+        Ok(Parser {
+            name_regex,
+        })
+    }
+
+    pub fn parse(&self, text: &str) -> Vec<Player> {
+        let name_captures = self.name_regex.captures_iter(text);
+
+        name_captures
+            .map(|captures| captures
+                .name("name")
+                .and_then(|result| Some(result.as_str()))
+                .map(|player_name|
+                    Player { name: player_name.to_string() }
+                )
+            )
+            .filter_map(|p| p)
+            .collect::<Vec<Player>>()
+    }
+}
+
+pub struct Player {
+    name: String,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const HTML_FIXTURE: &'static str = r#"
+<tr class="top3 ">
+    <td>1</td>
+    <td>
+        <div class="lb-profile">
+            <div class="player">
+                <a style="color:#f17f1a;" href="/bf1/profile/pc/AuMreaching7King">AuMreaching7King</a>
+            </div>
+            <div class="icons">
+                <img class="flag" src="/images/flags/China.png" />
+            </div>
+        </div>
+    </td>
+    <td>
+        <div class="pull-left">
+            <small class="text-success">Top 1%</small>
+        </div>
+        <div class="pull-right">
+            373,615 </div>
+    </td>
+    <td class="right">13520</td>
+</tr>
+<tr class="top3 ">
+    <td>2</td>
+    <td>
+        <div class="lb-profile">
+            <div class="player">
+                <a style="color:#f17f1a;" href="/bf1/profile/pc/Silmary_Fr">Silmary_Fr</a>
+            </div>
+            <div class="icons">
+                <img class="flag" src="/images/flags/France.png" />
+            </div>
+        </div>
+    </td>
+</tr>
+    "#;
+
+    const EXPECTED_NAMES: [&str; 2] = ["AuMreaching7King", "Silmary_Fr"];
+
+    #[test]
+    fn it_fetches_the_player_name() {
+        let parser = Parser::new().unwrap();
+        let result = parser.parse(HTML_FIXTURE);
+
+        result
+            .iter()
+            .zip(EXPECTED_NAMES.iter())
+            .for_each(|(actual, expected)|
+                assert_eq!(actual.name, *expected)
+            );
+    }
+}
